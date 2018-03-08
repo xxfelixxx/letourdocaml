ed02f0a0b468d8daa89fe02e6f809f84870f7058
Felix Tubiana <felixtubiana@gmail.com>
2018-03-08 05:43:43 +0000

    Added area_of_ring to variables.ml

--- chapter02/variables.ml
+++ chapter02/variables.ml
@@ -5,8 +5,13 @@ let dashed_languages =
   let language_list = String.split languages ~on:',' in
   String.concat ~sep:"-" language_list
 
+let area_of_ring inner_radius outer_radius =
+  let pi = Float.acos (-1.) in
+  let area_of_circle r = pi *. r *. r in
+  area_of_circle outer_radius -. area_of_circle inner_radius
+  
 let () =
   printf "--- variables.ml ---\n";
-  printf "Replacing , with - : %s -> %s\n" languages dashed_languages
-
-
+  printf "Replacing , with - : %s -> %s\n" languages dashed_languages;
+  printf "Area of ring with inner radius 3 and outer radius 4 is %F\n"
+    (area_of_ring 3. 4.)
