9de805423d9cf2e689454590e6d0a46e2e397724
Felix Tubiana <felixtubiana@gmail.com>
2018-03-11 09:26:58 +0000

    Added chapter04/freq.ml

---
+++ chapter04/freq.ml
@@ -0,0 +1,21 @@
+open Core
+
+(* https://stackoverflow.com/questions/45029549/example-code-from-real-world-ocaml-doesnt-run-as-intended *)   
+let build_counts () =
+  In_channel.fold_lines In_channel.stdin ~init:[] ~f:(fun counts line ->
+      let count =
+        match List.Assoc.find counts ~equal:(=) line with
+        | None -> 0
+        | Some x -> x
+      in
+      List.Assoc.add counts ~equal:(=) line (count + 1)
+    )
+
+(* let () means pattern match that the right-hand side returns 'unit'
+   which is typical for functions with side-effects, i.e. printing *)  
+let () =
+  build_counts ()
+  |> List.sort ~cmp:(fun (_,x) (_,y) -> Int.descending x y)
+  |> (fun l -> List.take l 10)
+  |> List.iter ~f:(fun (line,count) -> printf "%3d: %s\n" count line)
+                                             
